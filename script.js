ee73f9b304fad8fc789fd706092a4b661eeeaa1e
High-Flying-Fox <161777532+High-Flying-Fox@users.noreply.github.com>
2024-03-28 20:23:18 -0300

    moving square

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-document.addEventListener("keypress", key());
 
-function key() {
-    console.log("down")
+let x_vel = 0
+
+window.addEventListener("keydown", event => {
+    if(event.key==="ArrowLeft"){moveleft()};
+    if(event.key==="ArrowRight"){moveright()}
+});
+
+function moveleft() {
+    x_vel -= 2
+}
+function moveright() {
+    x_vel += 2
+}
+
+while (true) {
+x_vel = x_vel * 0.8;
+let left = parseInt(window.getComputedStyle(fox).getPropertyValue("Left"));
+    fox.style.left += x_vel + "px"    
 }
