91f8561dd7f5ec183ad4e1acbaff24e98536388a
High-Flying-Fox <161777532+High-Flying-Fox@users.noreply.github.com>
2024-03-29 21:56:52 -0300

    buff

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-window.addEventListener("keydown", event => {
-    if(event.key==="ArrowLeft"){moveleft()};
-    if(event.key==="ArrowRight"){moveright()}
-});
 
-function moveleft() {
-    let left = parseInt(window.getComputedStyle(fox).getPropertyValue("Left"));
-    left -= 10;
-    fox.style.left = left + "px"
-}
-function moveright() {
-    let left = parseInt(window.getComputedStyle(fox).getPropertyValue("Left"));
-    left += 10;
-    fox.style.left = left + "px"
-}
+let mousex
+let mousey
+
+window.addEventListener("mousemove", event => {
+    mousex = event.clientX
+    mousey = event.clientY
+    fox.style.left = (mousex -= 114) + "px"
+    fox.style.top = (mousey -= 114) + "px"
+})
